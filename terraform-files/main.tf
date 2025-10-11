95452ce69f8b1b8936a95e140482ad84b1a53428
Gautirockz <gowthamponnathi@gmail.com>
2025-10-12 04:46:56 +0530

    Update main.tf

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -1,38 +1,24 @@
-variable "private_key_path" {
-  description = "Path to PEM file for SSH connection"
-}
-
 resource "aws_instance" "test-server" {
-  ami                    = "ami-052064a798f08f0d3"
-  instance_type          = "t2.micro"
-  key_name               = "saijenkins1"
+  ami = "ami-0360c520857e3138f"
+  instance_type = "t2.micro"
+  key_name = "saijenkins1"
   vpc_security_group_ids = ["sg-0040ea524604db821"]
-
   connection {
-    type        = "ssh"
-    user        = "ubuntu"
-    private_key = file(var.private_key_path)
-    host        = self.public_ip
-    timeout     = "5m"
-  }
-
+     type = "ssh"
+     user = "ubuntu"
+     private_key = file("./saiubuntu.pem")
+     host = self.public_ip
+     }
   provisioner "remote-exec" {
-    inline = [
-      "echo '⏳ Waiting for instance to be ready for SSH...'",
-      "sleep 60",
-      "sudo apt-get update -y",
-      "sudo apt-get install -y nginx",
-      "sudo systemctl start nginx",
-      "sudo systemctl enable nginx",
-      "echo '✅ Nginx installed successfully!'"
-    ]
+     inline = ["echo 'wait to start the instance' "]
   }
-
   tags = {
-    Name = "test-server"
-  }
-
+     Name = "test-server"
+     }
   provisioner "local-exec" {
-    command = "echo ${self.public_ip} > inventory"
+     command = "echo ${aws_instance.test-server.public_ip} > inventory"
+     }
+  provisioner "local-exec" {
+     command = "ansible-playbook /var/lib/jenkins/workspace/BankingProject/terraform-files/ansibleplaybook.yml"
+     }
   }
-}
