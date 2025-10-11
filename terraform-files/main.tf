74fd96521ca04edc97b1f9ce4d8f69d34f5f6f05
Gautirockz <gowthamponnathi@gmail.com>
2025-10-11 20:44:30 +0530

    Update main.tf

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" "test-server" {
   ami = "ami-052064a798f08f0d3"
   instance_type = "t2.micro"
-  key_name = "saijenkins2"
+  key_name = "saijenkins1"
   vpc_security_group_ids = ["sg-0040ea524604db821"] 
   connection {
      type = "ssh"
      user = "ubuntu"
-     private_key = file("./saijenkins2.pem")
+     private_key = file("./saijenkins1.pem")
      host = self.public_ip
      }
   provisioner "remote-exec" {
